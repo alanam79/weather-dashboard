db7f449ad95c97aa19e1cf3506359b3d55901536
alanam79 <halo2382001@gmail.com>
2022-05-06 17:34:56 -0400

    adding apiURL to page

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,14 @@
-var getUserRepos = function () {
-    fetchfetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function (user) {
+  // format the api url
+  var apiUrl =
+    "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=dbbe680cb39d3e2a4fbbb13d470ca5cc"
+
+  // make a request to the url
+  fetch(apiUrl).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+    });
+  });
 };
 
 getUserRepos();
