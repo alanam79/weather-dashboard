acfe8b5f5e444d43d3f3d65a4420184de8b10399
alanam79 <halo2382001@gmail.com>
2022-05-06 12:26:07 -0400

    texted js functionality

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,5 @@
 var getUserRepos = function () {
-  console.log("function was called");
+    fetch("http://api.openweathermap.org/data/2.5/onecall/timemachine?lat=60.99&lon=30.9&dt=1586468027&appid=dbbe680cb39d3e2a4fbbb13d470ca5cc");
 };
 
 getUserRepos();
