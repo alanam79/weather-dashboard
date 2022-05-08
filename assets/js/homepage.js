c03df2f8d04b5e53e6638f45ced7327e0724a9d8
alanam79 <halo2382001@gmail.com>
2022-05-08 14:39:17 -0400

    obtaining information for weather

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -5,7 +5,7 @@ var cityContainerEl = document.querySelector("#repos-container");
 var citySearchTerm = document.querySelector("#repo-search-term");
 var recentSearches = JSON.parse(localStorage.getItem("recents") || "[]");
 
-var formSubmitHandler = function(event) {
+var formSubmitHandler = function (event) {
   // prevent page from refreshing
   event.preventDefault();
 
@@ -23,7 +23,7 @@ var formSubmitHandler = function(event) {
   }
 };
 
-var buttonClickHandler = function(event) {
+var buttonClickHandler = function (event) {
   // get the language attribute from the clicked element
   var language = event.target.getAttribute("data-language");
 
@@ -36,46 +36,51 @@ var buttonClickHandler = function(event) {
   }
 };
 
-var getWeather = function(city) {
-  // format the github api url
-  var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&units=imperial&appid=dbbe680cb39d3e2a4fbbb13d470ca5cc";
+var getWeather = function (cityLat, cityLon) {
+  var apiUrl =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    cityLat +
+    "&lon=" +
+    cityLon +
+    "&units=imperial&appid=dbbe680cb39d3e2a4fbbb13d470ca5cc";
 
   // make a get request to url
   fetch(apiUrl)
-    .then(function(response) {
+    .then(function (response) {
       // request was successful
       if (response.ok) {
         console.log(response);
-        response.json().then(function(data) {
+        response.json().then(function (data) {
           console.log(data);
-          displaycitys(data, city);
+          displayCity(data, cityLat, cityLon);
         });
       } else {
         alert("Error: " + response.statusText);
       }
     })
-    .catch(function(error) {
+    .catch(function (error) {
       alert("Unable to find City");
     });
 };
 
-function setLocalStorage(city) {
-  if (recentSearches.indexOf(city) === -1) {
-    recentSearches.push(city);
-    localStorage.setItem("recents", JSON.stringify(recentSearches));
-  }
-}
+// function setLocalStorage(city) {
+//   if (recentSearches.indexOf(city) === -1) {
+//     recentSearches.push(city);
+//     localStorage.setItem("recents", JSON.stringify(recentSearches));
+//   }
+// }
 
-var getFeaturedcitys = function(language) {
+var getFeaturedcitys = function (language) {
   // format the github api url
-  var apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=dbbe680cb39d3e2a4fbbb13d470ca5cc";
+  var apiUrl =
+    "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=dbbe680cb39d3e2a4fbbb13d470ca5cc";
 
   // make a get request to url
-  fetch(apiUrl).then(function(response) {
+  fetch(apiUrl).then(function (response) {
     // request was successful
     if (response.ok) {
-      response.json().then(function(data) {
-        displaycitys(data.items, language);
+      response.json().then(function (data) {
+        displayCity(data.items, language);
       });
     } else {
       alert("Error: " + response.statusText);
@@ -83,53 +88,40 @@ var getFeaturedcitys = function(language) {
   });
 };
 
-var displaycitys = function(city, searchTerm) {
-  // check if api returned any city
-  if (city.length === 0) {
-    cityContainerEl.textContent = "No City found.";
-    // location.reload(); - refreshes page
-    return;
-  }
+var displayCity = function (data) {
+  console.log(data);
 
-  citySearchTerm.textContent = searchTerm;
-
-  // // loop over city
-  // for (var i = 0; i < city.length; i++) {
-  //   // format city name
-  //   var cityName = city[i].owner.login + "/" + city[i].name;
-
-    // create a space for each city
-    var cityEl = document.createElement("div");
-    cityEl.classList = "list-item flex-row justify-space-between align-center";
-    // cityEl.setAttribute("href", "./single-city.html?city=" + cityName);
-
-    // create a h2 element to hold city name
-    var titleEl = document.createElement("h2");
-    titleEl.textContent = cityName;
-
-    // append to container
-    cityEl.appendChild(titleEl);
-
-    // create a status element
-    var statusEl = document.createElement("h2");
-    statusEl.classList = "flex-row align-center";
-
-  //   // check if current city has issues or not
-  //   if (city[i].open_issues_count > 0) {
-  //     statusEl.innerHTML =
-  //       "<i class='fas fa-times status-icon icon-danger'></i>" + city[i].open_issues_count + " issue(s)";
-  //   } else {
-  //     statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-  //   }
-
-    // append to container
-    cityEl.appendChild(statusEl);
-
-    // append container to the dom
-    cityContainerEl.appendChild(cityEl);
+  var currentWeather = {
+    description: data.weather[0].description,
+    main: data.main,
+    wind: data.wind,
+    clouds: data.clouds,
+    coord: data.coord
   };
+  console.log(currentWeather);
+  weatherData.textContent = data.weather[0].description;
 
+  // create a space for each city
+  var cityEl = document.createElement("div");
+  cityContainerEl.classList = "list-item flex-row justify-space-between align-center";
 
+  // create a h2 element to hold city name
+  var titleEl = document.createElement("h2");
+  titleEl.classList = "list-item flex-row justify-space-between align-center";
+
+  // append to container
+  cityContainerEl.appendChild(titleEl);
+
+  // create a h2 element
+  var statusEl = document.createElement("h2");
+  statusEl.classList = "flex-row align-center";
+
+  // append to container
+  cityEl.appendChild(statusEl);
+
+  // append container to the dom
+  cityContainerEl.appendChild(cityEl);
+};
 
 // add event listeners to form and button container
 userFormEl.addEventListener("submit", formSubmitHandler);
