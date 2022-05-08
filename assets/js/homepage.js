7f487a6108a4685cda752af6f560780eccc01763
alanam79 <halo2382001@gmail.com>
2022-05-08 12:40:16 -0400

    working on getting information showing on page

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,8 +1,9 @@
 var userFormEl = document.querySelector("#user-form");
 var languageButtonsEl = document.querySelector("#language-buttons");
 var nameInputEl = document.querySelector("#username");
-var repoContainerEl = document.querySelector("#repos-container");
-var repoSearchTerm = document.querySelector("#repo-search-term");
+var cityContainerEl = document.querySelector("#repos-container");
+var citySearchTerm = document.querySelector("#repo-search-term");
+var recentSearches = JSON.parse(localStorage.getItem("recents") || "[]");
 
 var formSubmitHandler = function(event) {
   // prevent page from refreshing
@@ -15,7 +16,7 @@ var formSubmitHandler = function(event) {
     getWeather(city);
 
     // clear old content
-    repoContainerEl.textContent = "";
+    cityContainerEl.textContent = "";
     nameInputEl.value = "";
   } else {
     alert("Please enter a City");
@@ -27,10 +28,11 @@ var buttonClickHandler = function(event) {
   var language = event.target.getAttribute("data-language");
 
   if (language) {
-    getFeaturedRepos(language);
+    getFeaturedcitys(language);
 
     // clear old content
-    repoContainerEl.textContent = "";
+    cityContainerEl.textContent = "";
+    citySearchTerm.textContent = searchTerm;
   }
 };
 
@@ -46,7 +48,7 @@ var getWeather = function(city) {
         console.log(response);
         response.json().then(function(data) {
           console.log(data);
-          displayRepos(data, city);
+          displaycitys(data, city);
         });
       } else {
         alert("Error: " + response.statusText);
@@ -64,7 +66,7 @@ function setLocalStorage(city) {
   }
 }
 
-var getFeaturedRepos = function(language) {
+var getFeaturedcitys = function(language) {
   // format the github api url
   var apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=dbbe680cb39d3e2a4fbbb13d470ca5cc";
 
@@ -73,7 +75,7 @@ var getFeaturedRepos = function(language) {
     // request was successful
     if (response.ok) {
       response.json().then(function(data) {
-        displayRepos(data.items, language);
+        displaycitys(data.items, language);
       });
     } else {
       alert("Error: " + response.statusText);
@@ -81,51 +83,53 @@ var getFeaturedRepos = function(language) {
   });
 };
 
-var displayRepos = function(repos, searchTerm) {
-  // check if api returned any repos
-  if (repos.length === 0) {
-    repoContainerEl.textContent = "No City found.";
+var displaycitys = function(city, searchTerm) {
+  // check if api returned any city
+  if (city.length === 0) {
+    cityContainerEl.textContent = "No City found.";
+    // location.reload(); - refreshes page
     return;
   }
 
-  repoSearchTerm.textContent = searchTerm;
+  citySearchTerm.textContent = searchTerm;
 
-  // loop over repos
-  for (var i = 0; i < repos.length; i++) {
-    // format repo name
-    var repoName = repos[i].owner.login + "/" + repos[i].name;
+  // // loop over city
+  // for (var i = 0; i < city.length; i++) {
+  //   // format city name
+  //   var cityName = city[i].owner.login + "/" + city[i].name;
 
-    // create a link for each repo
-    var repoEl = document.createElement("a");
-    repoEl.classList = "list-item flex-row justify-space-between align-center";
-    repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
+    // create a space for each city
+    var cityEl = document.createElement("div");
+    cityEl.classList = "list-item flex-row justify-space-between align-center";
+    // cityEl.setAttribute("href", "./single-city.html?city=" + cityName);
 
-    // create a span element to hold repository name
-    var titleEl = document.createElement("span");
-    titleEl.textContent = repoName;
+    // create a h2 element to hold city name
+    var titleEl = document.createElement("h2");
+    titleEl.textContent = cityName;
 
     // append to container
-    repoEl.appendChild(titleEl);
+    cityEl.appendChild(titleEl);
 
     // create a status element
-    var statusEl = document.createElement("span");
+    var statusEl = document.createElement("h2");
     statusEl.classList = "flex-row align-center";
 
-    // check if current repo has issues or not
-    if (repos[i].open_issues_count > 0) {
-      statusEl.innerHTML =
-        "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
-    } else {
-      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-    }
+  //   // check if current city has issues or not
+  //   if (city[i].open_issues_count > 0) {
+  //     statusEl.innerHTML =
+  //       "<i class='fas fa-times status-icon icon-danger'></i>" + city[i].open_issues_count + " issue(s)";
+  //   } else {
+  //     statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+  //   }
 
     // append to container
-    repoEl.appendChild(statusEl);
+    cityEl.appendChild(statusEl);
 
     // append container to the dom
-    repoContainerEl.appendChild(repoEl);
-  }
-};
+    cityContainerEl.appendChild(cityEl);
+  };
+
+
 
 // add event listeners to form and button container
 userFormEl.addEventListener("submit", formSubmitHandler);
